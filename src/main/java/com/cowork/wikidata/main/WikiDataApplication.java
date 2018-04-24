85bb343008397f2df827e9c0ca873633478901a9
yankj12 <yankj12@163.com>
2018-04-25 01:44:08 +0800

    服务注册到eureka中

--- src/main/java/com/cowork/wikidata/main/WikiDataApplication.java
+++ src/main/java/com/cowork/wikidata/main/WikiDataApplication.java
@@ -3,9 +3,11 @@ package com.cowork.wikidata.main;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@EnableDiscoveryClient
 @Configuration
 @ComponentScan("com.cowork")
 @EnableAutoConfiguration
